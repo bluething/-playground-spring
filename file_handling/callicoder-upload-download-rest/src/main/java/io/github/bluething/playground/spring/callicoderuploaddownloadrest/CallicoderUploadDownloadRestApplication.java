9d66f1370fa3c8be9f48fb87b8c5936ba49a5819
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-10 22:54:14 +0700

    Init module

---
+++ file_handling/callicoder-upload-download-rest/src/main/java/io/github/bluething/playground/spring/callicoderuploaddownloadrest/CallicoderUploadDownloadRestApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.callicoderuploaddownloadrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallicoderUploadDownloadRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallicoderUploadDownloadRestApplication.class, args);
+	}
+
+}
