d74d6df290dc2d10fc0f505d1e699ed2a5e7e1ec
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-10 17:16:19 +0700

    Clean up when start

--- gs_uploading_files/src/main/java/io/github/bluething/playground/spring/gs/uploading/files/GsUploadingFilesApplication.java
+++ gs_uploading_files/src/main/java/io/github/bluething/playground/spring/gs/uploading/files/GsUploadingFilesApplication.java
@@ -1,9 +1,12 @@
 package io.github.bluething.playground.spring.gs.uploading.files;
 
+import io.github.bluething.playground.spring.gs.uploading.files.file.Storage;
 import io.github.bluething.playground.spring.gs.uploading.files.file.StorageProperties;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -11,6 +14,14 @@ public class GsUploadingFilesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GsUploadingFilesApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner init(Storage storage) {
+		return (args -> {
+			storage.deleteAll();
+			storage.init();
+		});
 	}
 
 }
