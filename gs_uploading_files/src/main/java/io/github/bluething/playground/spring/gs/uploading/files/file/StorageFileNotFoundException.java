cfef7dc159e356f0f8cc28d0e2cf92862686dc58
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-10 17:15:23 +0700

    Define an exception

---
+++ gs_uploading_files/src/main/java/io/github/bluething/playground/spring/gs/uploading/files/file/StorageFileNotFoundException.java
@@ -0,0 +1,11 @@
+package io.github.bluething.playground.spring.gs.uploading.files.file;
+
+public class StorageFileNotFoundException extends StorageException {
+    public StorageFileNotFoundException(String message) {
+        super(message);
+    }
+
+    public StorageFileNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
