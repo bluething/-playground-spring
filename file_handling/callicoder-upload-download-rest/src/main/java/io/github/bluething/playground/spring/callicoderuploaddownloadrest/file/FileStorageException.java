af8ffbd312c3cecbd04cbed1cd50d6e8228981e9
Habib Ali Machpud <habib.machpud@gmail.com>
2023-03-10 23:13:12 +0700

    Define an exception

---
+++ file_handling/callicoder-upload-download-rest/src/main/java/io/github/bluething/playground/spring/callicoderuploaddownloadrest/file/FileStorageException.java
@@ -0,0 +1,11 @@
+package io.github.bluething.playground.spring.callicoderuploaddownloadrest.file;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
