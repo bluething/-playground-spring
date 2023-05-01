c7d9c53f1725e031a01514c8fb335c0bcb2f5ec8
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-01 16:50:40 +0700

    New module

---
+++ versioning/versioning-hexagon-arch/src/main/java/io/github/bluething/playground/spring/versioning/hexagonarch/VersioningHexagonArchApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.versioning.hexagonarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VersioningHexagonArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VersioningHexagonArchApplication.class, args);
+	}
+
+}
---
+++ versioning/versioning-hexagon-arch/src/test/java/io/github/bluething/playground/spring/versioning/hexagonarch/VersioningHexagonArchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.versioning.hexagonarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VersioningHexagonArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
