d286e4c23304b3a8a77c21235ade9340975f7497
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-22 23:03:01 +0700

    Job with fixed rate but take longer than time window but with async execution

--- scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/jobs/ScheduledJobs.java
+++ scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/jobs/ScheduledJobs.java
@@ -2,6 +2,7 @@ package io.github.bluething.playground.spring.scheduler.reflectoringscheduler.jo
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -26,6 +27,13 @@ class ScheduledJobs {
     @Scheduled(fixedRate = 3000)
     void reportCurrentTimeFixedRateAndSleep() throws InterruptedException {
         LOG.info("reportCurrentTimeFixedRateAndSleep - The time is now {}", sdf.format(new Date()));
+        Thread.sleep(4000);
+    }
+
+    @Scheduled(fixedRate = 3000)
+    @Async
+    void reportCurrentTimeFixedRateAndSleepWithAsync() throws InterruptedException {
+        LOG.info("reportCurrentTimeFixedRateAndSleepWithAsync - The time is now {}", sdf.format(new Date()));
         Thread.sleep(4000);
     }
 }
