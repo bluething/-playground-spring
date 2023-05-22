6f83da4f0947fcd0a30264489524e1f421c789a7
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-22 22:57:07 +0700

    Add conditional on property
    Using matchIfMissing = true so we don't need the property exist to run the scheduler. Only when we need to disable the scheduler, for example in testing.

--- scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/config/SchedulerConfig.java
+++ scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/config/SchedulerConfig.java
@@ -1,9 +1,11 @@
 package io.github.bluething.playground.spring.scheduler.reflectoringscheduler.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
+@ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
 class SchedulerConfig {
 }
