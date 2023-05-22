7badec2894df9b6d0b14fe7057a38627989a2361
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-22 22:50:18 +0700

    Init module

---
+++ scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/ReflectoringSchedulerApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.scheduler.reflectoringscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReflectoringSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReflectoringSchedulerApplication.class, args);
+	}
+
+}
---
+++ scheduler/reflectoring-scheduler/src/test/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/ReflectoringSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.scheduler.reflectoringscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReflectoringSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
