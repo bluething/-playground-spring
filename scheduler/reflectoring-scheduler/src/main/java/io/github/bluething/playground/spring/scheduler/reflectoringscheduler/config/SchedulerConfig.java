c690340d0e8923ca7a6ba2a7d097fb800d585128
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-22 23:37:12 +0700

    Configure shedlock

--- scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/config/SchedulerConfig.java
+++ scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/config/SchedulerConfig.java
@@ -1,13 +1,29 @@
 package io.github.bluething.playground.spring.scheduler.reflectoringscheduler.config;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "10m")
 @ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
 @EnableAsync
 class SchedulerConfig {
+
+    @Bean
+    LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(JdbcTemplateLockProvider.Configuration.builder()
+                .withJdbcTemplate(new JdbcTemplate(dataSource))
+                .usingDbTime()
+                .build());
+    }
 }
