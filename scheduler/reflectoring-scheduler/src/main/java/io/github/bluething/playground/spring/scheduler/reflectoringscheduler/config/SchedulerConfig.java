99ec4923300cd49270c49650e6dd7ed3b4612b7e
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-22 23:12:19 +0700

    Enable async
    The objective is parallel job execution. The alternative way is set spring.task.scheduling.pool.size property, the default value is 1.

--- scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/config/SchedulerConfig.java
+++ scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/config/SchedulerConfig.java
@@ -2,10 +2,12 @@ package io.github.bluething.playground.spring.scheduler.reflectoringscheduler.co
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @Configuration
 @EnableScheduling
 @ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
+@EnableAsync
 class SchedulerConfig {
 }
