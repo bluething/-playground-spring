3abb85c95a686d726830f2220e6bcffad844fa46
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-22 22:55:02 +0700

    Job with fixed delay

---
+++ scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/config/SchedulerConfig.java
@@ -0,0 +1,9 @@
+package io.github.bluething.playground.spring.scheduler.reflectoringscheduler.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+class SchedulerConfig {
+}
---
+++ scheduler/reflectoring-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/reflectoringscheduler/jobs/ScheduledJobs.java
@@ -0,0 +1,20 @@
+package io.github.bluething.playground.spring.scheduler.reflectoringscheduler.jobs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+class ScheduledJobs {
+    private static final Logger LOG = LoggerFactory.getLogger(ScheduledJobs.class);
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedDelay = 2000)
+    void reportCurrentTimeFixedDelay() {
+        LOG.info("reportCurrentTimeFixedDelay - The time is now {}", sdf.format(new Date()));
+    }
+}
