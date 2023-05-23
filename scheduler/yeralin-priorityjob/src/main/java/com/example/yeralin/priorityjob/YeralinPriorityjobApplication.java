cdbdf95018ca8a70b05269702798696cced642ab
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-23 09:48:27 +0700

    New module

---
+++ scheduler/yeralin-priorityjob/src/main/java/com/example/yeralin/priorityjob/YeralinPriorityjobApplication.java
@@ -0,0 +1,13 @@
+package com.example.yeralin.priorityjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeralinPriorityjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeralinPriorityjobApplication.class, args);
+	}
+
+}
---
+++ scheduler/yeralin-priorityjob/src/test/java/com/example/yeralin/priorityjob/YeralinPriorityjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yeralin.priorityjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeralinPriorityjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
