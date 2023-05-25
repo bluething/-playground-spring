95df239fb23be9385579805c51acd31d3fbe731a
Habib Ali Machpud <habib.machpud@gmail.com>
2023-05-25 11:05:16 +0700

    Init module

---
+++ scheduler/technicalsand-scheduler/src/main/java/io/github/bluething/playground/spring/scheduler/technicalsandscheduler/TechnicalsandSchedulerApplication.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.scheduler.technicalsandscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalsandSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnicalsandSchedulerApplication.class, args);
+	}
+
+}
