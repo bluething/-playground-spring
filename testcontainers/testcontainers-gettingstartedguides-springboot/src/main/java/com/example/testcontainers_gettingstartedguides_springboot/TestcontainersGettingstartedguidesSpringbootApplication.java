3fea992e36c6d34612461af7ee1e4250134db46e
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-18 17:28:58 +0700

    Init

---
+++ testcontainers/testcontainers-gettingstartedguides-springboot/src/main/java/com/example/testcontainers_gettingstartedguides_springboot/TestcontainersGettingstartedguidesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers_gettingstartedguides_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersGettingstartedguidesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersGettingstartedguidesSpringbootApplication.class, args);
+	}
+
+}
---
+++ testcontainers/testcontainers-gettingstartedguides-springboot/src/test/java/com/example/testcontainers_gettingstartedguides_springboot/TestcontainersGettingstartedguidesSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers_gettingstartedguides_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersGettingstartedguidesSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
