9a86baedeeb5f199bf4bd3bd16f3f95059916596
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-18 18:04:34 +0700

    Init

---
+++ testcontainers/testcontainers-gettingstartedguides-kafka/src/main/java/com/exampleio/github/bluething/playground/spring/testcontainers/testcontainers_gettingstartedguides_kafka/TestcontainersGettingstartedguidesKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.exampleio.github.bluething.playground.spring.testcontainers.testcontainers_gettingstartedguides_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersGettingstartedguidesKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersGettingstartedguidesKafkaApplication.class, args);
+	}
+
+}
---
+++ testcontainers/testcontainers-gettingstartedguides-kafka/src/test/java/com/exampleio/github/bluething/playground/spring/testcontainers/testcontainers_gettingstartedguides_kafka/TestcontainersGettingstartedguidesKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleio.github.bluething.playground.spring.testcontainers.testcontainers_gettingstartedguides_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersGettingstartedguidesKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
