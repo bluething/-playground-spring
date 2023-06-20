d75ed915385ea5617bb3786d338664916eabcb93
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-20 11:13:38 +0700

    Init localstack

---
+++ testcontainers/testcontainers-gettingstartedguides-localstack/src/test/java/io/github/bluething/playground/spring/testcontainers/gettingstartedguides_localstack/TestcontainersGettingstartedguidesLocalstackApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.testcontainers.gettingstartedguides_localstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersGettingstartedguidesLocalstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
