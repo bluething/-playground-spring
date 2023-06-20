33eec26a2ce9beb3f90051da78de25b56a2e3531
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-20 11:03:00 +0700

    Init mockserver

---
+++ testcontainers/testcontainers-gettingstartedguides-mockserver/src/test/java/io/github/bluething/playground/spring/testcontainers/gettingstartedguides_mockserver/TestcontainersGettingstartedguidesMockserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.testcontainers.gettingstartedguides_mockserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersGettingstartedguidesMockserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
