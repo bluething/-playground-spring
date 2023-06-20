2e468782873c0999006b65c4505d204d54cec7e2
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-20 11:18:26 +0700

    Init jooq flyway

---
+++ testcontainers/testcontainers-gettingstartedguides-jooq-flyway/src/test/java/io/github/bluething/playground/spring/testcontainers/gettingstartedguides_jooq_flyway/TestcontainersGettingstartedguidesJooqFlywayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.testcontainers.gettingstartedguides_jooq_flyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersGettingstartedguidesJooqFlywayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
