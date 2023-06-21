3a31f10016fae10a040e5f67fbdc85ca0632372d
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-21 23:10:39 +0700

    Add test

---
+++ testcontainers/testcontainers-gettingstartedguides-springboot/src/test/java/com/example/testcontainers_gettingstartedguides_springboot/CustomerControllerTest.java
@@ -0,0 +1,71 @@
+package com.example.testcontainers_gettingstartedguides_springboot;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.hasSize;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CustomerControllerTest {
+    @LocalServerPort
+    private Integer port;
+
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @BeforeAll
+    static void beforeAll() {
+        postgreSQLContainer.start();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        postgreSQLContainer.stop();
+    }
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        dynamicPropertyRegistry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        dynamicPropertyRegistry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
+
+    @Autowired
+    CustomerRepository repository;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost:" + port;
+        repository.deleteAll();
+    }
+
+    @Test
+    void shouldGetAllCustomers() {
+        List<Customer> customers = List.of(
+                new Customer(null, "John", "john@mail.com"),
+                new Customer(null, "Dennis", "dennis@mail.com")
+        );
+        repository.saveAll(customers);
+
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get("/api/customers")
+                .then()
+                .statusCode(200)
+                .body(".", hasSize(2));
+    }
+
+}
