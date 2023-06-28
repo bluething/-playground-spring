db53ccbb429428320a4b25446cddae843605e354
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-28 15:47:34 +0700

    Test with H2

---
+++ testcontainers/testcontainers-gettingstartedguides-h2/src/test/java/io/github/bluething/playground/spring/testcontainers/gettingstartedguides_h2/ProductRepositoryTest.java
@@ -0,0 +1,36 @@
+package io.github.bluething.playground.spring.testcontainers.gettingstartedguides_h2;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+class ProductRepositoryTest {
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    @Sql("classpath:/sql/seed-data.sql")
+    void shouldGetAllProducts() {
+        List<Product> products = productRepository.findAll();
+        Assertions.assertEquals(2, products.size());
+    }
+
+    @Test
+    @Sql("classpath:/sql/seed-data.sql")
+    void whenExecuteWithH2ThenProductRepositoryShouldThrowInvalidDataAccessResourceUsageException() {
+        Throwable exception = assertThrows(InvalidDataAccessResourceUsageException.class, () -> productRepository.insertWithNativePostgres(100L, "code", "name"));
+        String exceptionMessage = """
+               could not prepare statement [Syntax error in SQL statement "INSERT INTO products(id, code, name) VALUES(?, ?, ?) [*]ON CONFLICT DO NOTHING;"; SQL statement:
+               INSERT INTO products(id, code, name) VALUES(?, ?, ?) ON CONFLICT DO NOTHING; [42000-214]] [INSERT INTO products(id, code, name) VALUES(?, ?, ?) ON CONFLICT DO NOTHING;]; SQL [INSERT INTO products(id, code, name) VALUES(?, ?, ?) ON CONFLICT DO NOTHING;]
+               """;
+        Assertions.assertEquals(exceptionMessage.trim(), exception.getMessage());
+    }
+}
