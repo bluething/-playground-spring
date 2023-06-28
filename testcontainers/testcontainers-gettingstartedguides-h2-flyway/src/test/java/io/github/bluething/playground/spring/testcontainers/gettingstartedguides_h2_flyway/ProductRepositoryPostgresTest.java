f3e6c8d5c7e5710da9f1afe5d6a18a65a5f75360
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-28 17:14:40 +0700

    Test with postgres

---
+++ testcontainers/testcontainers-gettingstartedguides-h2-flyway/src/test/java/io/github/bluething/playground/spring/testcontainers/gettingstartedguides_h2_flyway/ProductRepositoryPostgresTest.java
@@ -0,0 +1,36 @@
+package io.github.bluething.playground.spring.testcontainers.gettingstartedguides_h2_flyway;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.util.List;
+
+@DataJpaTest
+@TestPropertySource(properties = {
+        "spring.test.database.replace=none",
+        "spring.datasource.url=jdbc:tc:postgresql:15.2-alpine:///db",
+})
+class ProductRepositoryPostgresTest {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Test
+    @Sql("classpath:/sql/seed-data.sql")
+    void shouldGetAllProducts() {
+        List<Product> products = productRepository.findAll();
+        Assertions.assertEquals(2, products.size());
+    }
+
+    @Test
+    @Sql("classpath:/sql/seed-data.sql")
+    void whenExecuteWithTestcontainersPostgresThenProductRepositoryShouldSuccess() {
+        productRepository.insertWithNativePostgres(100L, "code", "name");
+        List<Product> products = productRepository.findAll();
+        Assertions.assertEquals(3, products.size());
+    }
+}
