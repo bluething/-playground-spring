4cba592a6fa74dd1940dc0d084a6e04f1e9c9e70
Habib Ali Machpud <habib.machpud@gmail.com>
2023-06-28 14:45:57 +0700

    Init

---
+++ testcontainers/testcontainers-gettingstartedguides-h2/src/test/java/io/github/bluething/playground/spring/testcontainers/gettingstartedguides_h2/TestcontainersGettingstartedguidesH2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bluething.playground.spring.testcontainers.gettingstartedguides_h2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersGettingstartedguidesH2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
